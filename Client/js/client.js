b8bdad28671f585783e4aba172d4806432af7972
TarunKoli <amankumar8348@gmail.com>
2022-05-29 21:59:30 +0530

    Added Online feature

--- Client/js/client.js
+++ Client/js/client.js
@@ -1,217 +1,229 @@
-const socket = io("http://localhost:8000");
+(() => {
+  const socket = io("http://localhost:8000");
 
-const send = document.querySelector(".plane");
-const msgInp = document.querySelector("#msgInp");
-const chatContainer = document.querySelector(".chats");
-const stat = document.querySelector(".status span");
-let Users = [];
-let currentRoom = {
-  roomId: "",
-  sender: {},
-  reciever: {},
-  messages: [],
-};
+  const send = document.querySelector(".plane");
+  const msgInp = document.querySelector("#msgInp");
+  const chatContainer = document.querySelector(".chats");
+  const stat = document.querySelector(".status span");
+  let Users = new Map();
+  let currentRoom = {
+    roomId: "",
+    sender: {},
+    reciever: {},
+    messages: [],
+  };
 
-// API's
+  // API's
 
-function renderConversations() {
-  fetch("http://localhost:8080/users")
-    .then((data) => {
-      return data.json();
-    })
-    .then((parsedData) => {
-      Users = parsedData.data;
-      Users.forEach((user) => {
-        createConversations(user);
+  function renderConversations() {
+    fetch("http://localhost:8080/users")
+      .then((data) => {
+        return data.json();
+      })
+      .then((parsedData) => {
+        parsedData.data.forEach((user) => {
+          Users.set(user.email, user);
+          createConversations(user);
+        });
       });
-    });
-}
-
-function login() {
-  const username = prompt("Enter Your Name to Join");
-  fetch("http://localhost:8080/login", {
-    method: "POST", // or 'PUT'
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      username: username,
-    }),
-  })
-    .then((res) => {
-      if (res.status === 200) renderConversations();
-
-      return res.json();
-    })
-    .then((parsedData) => {
-      socket.emit("new-user-joined", parsedData.user);
-      window.localStorage.setItem("user", parsedData.user.id);
-    });
-}
-
-login();
-
-// Sockets
-socket.on("user-joined", (user) => {
-  createConversations(user);
-});
-
-socket.on("recieve", (msg) => {
-  console.log(msg);
-  createMsg("rec", msg.text);
-});
-
-socket.on("left", (username) => {
-  stat.style.display = "none";
-});
-
-// utility functions
-
-send.addEventListener("click", () => {
-  sendMessage(msgInp.value);
-  // socket.emit("send", msgInp.value);
-});
-
-const createMsg = (type, msg) => {
-  const divi = document.createElement("div");
-  const person = document.createElement("div");
-  const image = document.createElement("img");
-  const msgWrap = document.createElement("div");
-  const para = document.createElement("p");
-
-  image.src = "./assets/user.jpg";
-  person.classList.add("person");
-  para.textContent = msg;
-
-  type === "rec"
-    ? msgWrap.classList.add("rec_msg")
-    : msgWrap.classList.add("sent_msg");
-
-  person.appendChild(image);
-  msgWrap.appendChild(para);
-
-  if (type === "rec") {
-    divi.appendChild(person);
-    divi.appendChild(msgWrap);
-    divi.classList.add("rec_wrapper");
-  } else {
-    divi.appendChild(msgWrap);
-    divi.appendChild(person);
-    divi.classList.add("sent_wrapper");
   }
 
-  chatContainer.appendChild(divi);
-};
+  function login() {
+    const username = prompt("Enter Your Name to Join");
+    const email = prompt("Enter Your Email");
+    const password = prompt("Enter Your Password");
 
-const createConversations = (user) => {
-  const Members = document.querySelector(".members");
-
-  const divi = document.createElement("div");
-  const wrapper = document.createElement("div");
-  const dots = document.createElement("div");
-  const singleDot = document.createElement("div");
-  const pic = document.createElement("div");
-  const name = document.createElement("div");
-  const time = document.createElement("div");
-  const p = document.createElement("p");
-  const span = document.createElement("span");
-  const image = document.createElement("img");
-  const h3 = document.createElement("h3");
-
-  divi.classList.add("people");
-  // divi.setAttribute("active", "true");
-  wrapper.classList.add("wrap");
-  dots.classList.add("dots");
-  pic.classList.add("pic");
-  name.classList.add("name");
-  time.classList.add("time");
-  singleDot.classList.add("single");
-
-  divi.append(wrapper, dots);
-  wrapper.append(pic, name);
-
-  image.src = "./assets/user.jpg";
-  h3.textContent = user.name;
-  pic.appendChild(image);
-  name.appendChild(h3);
-  name.appendChild(time);
-
-  p.textContent = "I am ready";
-  span.textContent = "6:30 pm";
-
-  time.append(p, span);
-
-  dots.appendChild(singleDot);
-
-  Members.appendChild(divi);
-
-  divi.addEventListener("click", () => {
-    // if (!divi.getAttribute("active")) {
-    // divi.setAttribute("active", "true");
-    fetch(`http://localhost:8080/room`, {
-      method: "POST",
+    fetch("http://localhost:8080/login", {
+      method: "POST", // or 'PUT'
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        reciever: user.id,
-        sender: window.localStorage.getItem("user"),
+        username: username,
+        email: email,
+        pass: password,
       }),
     })
       .then((res) => {
-        if (res.ok) {
+        if (res.status === 200) {
+          renderConversations();
           return res.json();
         }
       })
-      .then((data) => {
-        socket.emit("join-room", {
-          roomId: data.room.roomId,
-          oldRoom: currentRoom,
-        });
-        updateChatBox(data.room);
+      .then((parsedData) => {
+        socket.emit("new-user-joined", parsedData.user);
+        window.localStorage.setItem("user", parsedData.user.id);
       });
-    // }
-  });
-};
-
-const updateChatBox = (room) => {
-  if (!currentRoom || currentRoom.roomId !== room.roomId) {
-    const logedInUser = window.localStorage.getItem("user");
-    chatContainer.innerHTML = "";
-    currentRoom = room;
-
-    document.querySelector(".status h3").textContent =
-      room.reciever.id === logedInUser ? room.sender.name : room.reciever.name;
-
-    room.messages.forEach((msg) => {
-      if (msg.id === logedInUser) {
-        createMsg("sent", msg.text);
-      } else {
-        createMsg("rec", msg.text);
-      }
-    });
   }
-};
 
-const sendMessage = (msg) => {
-  const data = {
-    roomId: currentRoom.roomId,
-    userId: window.localStorage.getItem("user"),
-    msg: msg,
+  login();
+
+  // Sockets
+  socket.on("user-joined", (user) => {
+    if (!Users.has(user.email)) createConversations(user);
+  });
+
+  socket.on("recieve", (msg) => {
+    createMsg("rec", msg.text);
+  });
+
+  socket.on("left", (user) => {
+    Users.delete(user.email);
+    Users.set(user.email, user);
+  });
+
+  // utility functions
+
+  send.addEventListener("click", () => {
+    sendMessage(msgInp.value);
+  });
+
+  const createMsg = (type, msg) => {
+    const divi = document.createElement("div");
+    const person = document.createElement("div");
+    const image = document.createElement("img");
+    const msgWrap = document.createElement("div");
+    const para = document.createElement("p");
+
+    image.src = "./assets/user.jpg";
+    person.classList.add("person");
+    para.textContent = msg;
+
+    type === "rec"
+      ? msgWrap.classList.add("rec_msg")
+      : msgWrap.classList.add("sent_msg");
+
+    person.appendChild(image);
+    msgWrap.appendChild(para);
+
+    if (type === "rec") {
+      divi.appendChild(person);
+      divi.appendChild(msgWrap);
+      divi.classList.add("rec_wrapper");
+    } else {
+      divi.appendChild(msgWrap);
+      divi.appendChild(person);
+      divi.classList.add("sent_wrapper");
+    }
+
+    chatContainer.appendChild(divi);
   };
-  fetch("http://localhost:8080/message", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  })
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      socket.emit("send-msg", { roomId: currentRoom.roomId, msg: data.msg });
-      createMsg("sent", data.msg.text);
-      msgInp.value = "";
+
+  const createConversations = (user) => {
+    const Members = document.querySelector(".members");
+
+    const divi = document.createElement("div");
+    const wrapper = document.createElement("div");
+    const dots = document.createElement("div");
+    const singleDot = document.createElement("div");
+    const pic = document.createElement("div");
+    const name = document.createElement("div");
+    const time = document.createElement("div");
+    const p = document.createElement("p");
+    const span = document.createElement("span");
+    const image = document.createElement("img");
+    const h3 = document.createElement("h3");
+
+    divi.classList.add("people");
+    // divi.setAttribute("active", "true");
+    wrapper.classList.add("wrap");
+    dots.classList.add("dots");
+    pic.classList.add("pic");
+    name.classList.add("name");
+    time.classList.add("time");
+    singleDot.classList.add("single");
+
+    divi.append(wrapper, dots);
+    wrapper.append(pic, name);
+
+    image.src = "./assets/user.jpg";
+    h3.textContent = user.name;
+    pic.appendChild(image);
+    name.appendChild(h3);
+    name.appendChild(time);
+
+    p.textContent = "I am ready";
+    span.textContent = "6:30 pm";
+
+    time.append(p, span);
+
+    dots.appendChild(singleDot);
+
+    Members.appendChild(divi);
+
+    divi.addEventListener("click", () => {
+      fetch(`http://localhost:8080/room`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          reciever: user.id,
+          sender: window.localStorage.getItem("user"),
+        }),
+      })
+        .then((res) => {
+          if (res.ok) {
+            return res.json();
+          }
+        })
+        .then((data) => {
+          socket.emit("join-room", {
+            roomId: data.room.roomId,
+            oldRoom: currentRoom,
+          });
+          updateChatBox(data.room);
+        });
     });
-};
+  };
+
+  const updateChatBox = (room) => {
+    if (!currentRoom || currentRoom.roomId !== room.roomId) {
+      const logedInUser = window.localStorage.getItem("user");
+      chatContainer.innerHTML = "";
+      currentRoom = room;
+
+      document.querySelector(".status h3").textContent =
+        room.reciever.id === logedInUser
+          ? room.sender.name
+          : room.reciever.name;
+
+      if (room.reciever.id === logedInUser) {
+        stat.style.display = room.sender.status ? "block" : "none";
+      } else {
+        stat.style.display = room.reciever.status ? "block" : "none";
+      }
+
+      room.messages.forEach((msg) => {
+        if (msg.id === logedInUser) {
+          createMsg("sent", msg.text);
+        } else {
+          createMsg("rec", msg.text);
+        }
+      });
+    }
+  };
+
+  const sendMessage = (msg) => {
+    const data = {
+      roomId: currentRoom.roomId,
+      userId: window.localStorage.getItem("user"),
+      msg: msg,
+    };
+    fetch("http://localhost:8080/message", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        socket.emit("send-msg", { roomId: currentRoom.roomId, msg: data.msg });
+        createMsg("sent", data.msg.text);
+        msgInp.value = "";
+      });
+  };
+})();
--- Server/server.js
+++ Server/server.js
@@ -14,9 +14,21 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.post("/login", (req, res) => {
-  const user = {};
+  let user = users.find((person) => {
+    return person.email === req.body.email;
+  });
+
+  if (user) {
+    if (user.password === req.body.pass) return res.status(200).json({ user });
+    else return res.status(404).json({ msg: "Wrong Password" });
+  }
+
+  user = {};
   user.id = uuid.v1();
   user.name = req.body.username;
+  user.email = req.body.email;
+  user.password = req.body.pass;
+  user.status = true;
   user.friends = [];
   user.rooms = [];
   users.push(user);
@@ -45,12 +57,21 @@ app.post("/room", (req, res) => {
     );
   });
 
-  if (room.length > 0) return res.status(200).json({ room: room[0] });
+  if (room.length > 0) {
+    if (room[0].sender.id === senderId && room[0].reciever.id === recieverId) {
+      room[0].sender.status = sender.status;
+      room[0].reciever.status = reciever.status;
+    } else {
+      room[0].sender.status = reciever.status;
+      room[0].reciever.status = sender.status;
+    }
+    return res.status(200).json({ room: room[0] });
+  }
 
   room = {
     roomId: "room_" + uuid.v1(),
-    sender: { id: sender.id, name: sender.name },
-    reciever: { id: reciever.id, name: reciever.name },
+    sender: { id: sender.id, name: sender.name, status: sender.status },
+    reciever: { id: reciever.id, name: reciever.name, status: reciever.status },
     messages: [],
   };
 
@@ -77,6 +98,10 @@ app.post("/message", (req, res) => {
 
 io.on("connection", (socket) => {
   socket.on("new-user-joined", (user) => {
+    const index = users.findIndex((person) => person.id === user.id);
+    users[index].socketId = socket.id;
+    users[index].status = true;
+    user = users[index];
     socket.broadcast.emit("user-joined", user);
   });
   socket.on("join-room", ({ roomId, oldRoom }) => {
@@ -85,6 +110,13 @@ io.on("connection", (socket) => {
   });
   socket.on("send-msg", ({ roomId, msg }) => {
     socket.to(roomId).emit("recieve", msg);
+  });
+  socket.on("disconnect", () => {
+    const index = users.findIndex((person) => person.socketId === socket.id);
+    if (index > -1) {
+      users[index].status = false;
+      socket.broadcast.emit("left", users[index]);
+    }
   });
 });
 
